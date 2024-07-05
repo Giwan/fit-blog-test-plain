7dd2374b8b44e2e749b41eaa0c31ea0e55fb1170
Giwan Persaud <giwan.persaud@adevinta.com>
2024-07-05 16:15:42 +0200

    fix: gh workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://giwan.github.io',
+	base: 'fit-blog-test-plain',
 	integrations: [mdx(), sitemap()],
 });
